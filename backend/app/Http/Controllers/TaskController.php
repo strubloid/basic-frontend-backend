1d5db2aba10c7cabec928319b3803cbfd0055df9
strubloid <strubloid@gmail.com>
2020-10-27 13:51:18 +0000

    finished the backedn call for the TaskController with add action working

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
-    //
+        public function add(Request $request)
+        {
+            $title = $request->input('title');
+            $status = $request->input('status');
+            $date = $request->input('date');
+
+            $task = new Task();
+            $task->title = $title;
+            $task->status = $status;
+            $task->date = $date;
+
+            $task->save();
+
+            return $task;
+        }
 }
